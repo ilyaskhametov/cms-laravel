5fe49b076a3c01f709f3a27ef0ae2b020cd8e183
Ilyas Khametov <ilyas.khametov@gmail.com>
2020-04-01 09:53:21 +0600

    Add browserSync | Add permanent storage for postgres on Windows

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -14,4 +14,11 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
 
+mix.browserSync({
+    proxy: 'nginx',
+    open: false,
+    ui: false,
+    port: 8080,
+});
+
 mix.copyDirectory('resources/images', 'public/images');
